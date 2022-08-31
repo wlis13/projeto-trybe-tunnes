1d6870dada82215a813b544ac73ab0ba589f12b1
wlisses <wlissesfernando285@gmail.com>
2022-08-31 12:52:40 -0300

    foi retirado um this.props de match, params e id do arquivo Album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 class Album extends Component {
   render() {
-    // eslint-disable-next-line react/prop-types
-    const { match } = this.props;
-    // eslint-disable-next-line react/prop-types
-    const { params } = match;
-    // eslint-disable-next-line react/prop-types
-    const { id } = params;
     return (
       <div data-testid="page-album">
         Este é o componente Album
-        <p>{id}</p>
       </div>
     );
   }
 }
 
 export default Album;
-
-Album.propType = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
